597d046b15497527d98c1a6de5e70946f9236ecf
xDaryamo <mr.dario.mazza@gmail.com>
2026-01-05 22:23:58 +0000

    fix: restructure Vite configuration for server and preview settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,14 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  vite: {
-    server: {
-      allowedHosts: true
-    },
-    preview: {
-      allowedHosts: true 
-    }
+ 
+  server: {
+    host: true,        
+    allowedHosts: true  
+  },
+
+  preview: {
+    host: true,
+    allowedHosts: true
   }
 });
