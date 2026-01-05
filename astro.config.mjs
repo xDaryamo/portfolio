1b749165ae2196862af31fe2d54ebb7ab0884147
Dario Mazza <63303412+xDaryamo@users.noreply.github.com>
2026-01-05 23:03:01 +0100

    fix: configure Vite server and preview allowed hosts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    server: {
+      allowedHosts: true
+    },
+    preview: {
+      allowedHosts: true 
+  }
+});
